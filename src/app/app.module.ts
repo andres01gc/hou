f2d6ff70a17d14bda1845eb847352ec2be5fbd1d
gc <anfgc01@gmail.com>
2018-03-10 21:06:14 -0500

    all working good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ActividadComponent} from './mainroutes/actividad/actividad.component';
+import {RouterModule, Routes} from '@angular/router';
+import {PacienteComponent} from './mainroutes/paciente/paciente.component';
+import {IngresarPacienteComponent} from './mainroutes/ingresar-paciente/ingresar-paciente.component';
 
+const appRoutes: Routes = [
+  {path: 'paciente', component: PacienteComponent},
+  {path: 'nuevo_ingreso', component: IngresarPacienteComponent},
+  {path: 'inicio', component: ActividadComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActividadComponent,
+    PacienteComponent,
+    IngresarPacienteComponent
   ],
-  imports: [
+  imports: [RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
