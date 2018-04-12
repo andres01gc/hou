93d91328cbaf733d4e573459d0b60a9454e3df38
gc <anfgc01@gmail.com>
2018-04-12 09:24:38 -0500

    se agregan los nuevos componentes del perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {AppComponent} from './app.component';
 import {ServiciosComponent} from './main_routes/home/main/perfil/serviciosC/inicio.component';
 import {RouterModule, Routes} from '@angular/router';
 import {PacienteComponent} from './main_routes/paciente/paciente.component';
-import {IngresarPacienteComponent} from './ingresar-paciente/ingresar-paciente.component';
+import {IngresarPacienteComponent} from './main_routes/ingresar-paciente/ingresar-paciente.component';
 import {CitasComponent} from './main_routes/home/main/perfil/citas/citas.component';
 import {CardServicioComponent} from './main_routes/home/main/perfil/serviciosC/card-servicio/card-servicio.component';
 import {InputComponent} from './utils/input/input.component';
@@ -20,23 +20,23 @@ import {NavbarComponent} from './main_routes/home/main/navbar/navbar.component';
 import {DataService} from './services/data.service';
 import {MainComponent} from './main_routes/home/main/main.component';
 import {PerfilComponent} from './main_routes/home/main/perfil/perfil.component';
-import {ConfiguracionesComponent} from './configuraciones/configuraciones.component';
+import {ConfiguracionesComponent} from './main_routes/configuraciones/configuraciones.component';
 import {MiscComponent} from './misc/misc.component';
 import {AnexosComponent} from './anexos/anexos.component';
 import {HistoriaComponent} from './historia/historia.component';
 import {ResumenComponent} from './resumen/resumen.component';
 import {RegistroComponent} from './registro/registro.component';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
-import {InicioComponent} from './inicio/inicio.component';
+import {InicioComponent} from './main_routes/inicio/inicio.component';
 import {SelectComponent} from './utils/select/select.component';
 import {ServiciosPacienteComponent} from './servicios-paciente/servicios-paciente.component';
 import {PgOdontogramaComponent} from './pg-odontograma/pg-odontograma.component';
 import {PgServicioComponent} from './pg-servicio/pg-servicio.component';
 import {TabsComponent} from './tabs/tabs.component';
 import {TabComponent} from './tab/tab.component';
-import {InfoSujetoComponent} from './info-sujeto/info-sujeto.component';
+import {InfoSujetoComponent} from './items/info-sujeto/info-sujeto.component';
 import {HcBasicComponent} from './historia/hc-pages/hc-basic/hc-basic.component';
-import {ItemTelComponent} from './item-tel/item-tel.component';
+import {ItemTelComponent} from './items/item-tel/item-tel.component';
 import {HcAnamnesisComponent} from './historia/hc-pages/hc-anamnesis/hc-anamnesis.component';
 import {HcAntecedentesComponent} from './historia/hc-pages/hc-antecedentes/hc-antecedentes.component';
 import {HcHabitosComponent} from './historia/hc-pages/hc-habitos/hc-habitos.component';
@@ -51,10 +51,14 @@ import {HcDiagnosticoComponent} from './historia/hc-pages/hc-diagnostico/hc-diag
 import {HcDiagnosticoPorDienteComponent} from './historia/hc-pages/hc-diagnostico-por-diente/hc-diagnostico-por-diente.component';
 import {HcTratamientoComponent} from './historia/hc-pages/hc-tratamiento/hc-tratamiento.component';
 import {HcConsentimientoComponent} from './historia/hc-pages/hc-consentimiento/hc-consentimiento.component';
-import {ItemReferenciaComponent} from './item-referencia/item-referencia.component';
-import { CntComponent } from './cnt/cnt.component';
-import { ItemDirComponent } from './item-dir/item-dir.component';
-import { AppCheckboxComponent } from './app-checkbox/app-checkbox.component';
+import {ItemReferenciaComponent} from './items/item-referencia/item-referencia.component';
+import {CntComponent} from './cnt/cnt.component';
+import {ItemDirComponent} from './items/item-dir/item-dir.component';
+import {AppCheckboxComponent} from './app-checkbox/app-checkbox.component';
+import {MultipleComponent} from './items/multiple/multiple.component';
+import {UserServiciosComponent} from './user-servicios/user-servicios.component';
+import {UserCitasComponent} from './user-citas/user-citas.component';
+import {UserResumenComponent} from './user-resumen/user-resumen.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -65,9 +69,8 @@ const appRoutes: Routes = [
         path: '', component: InicioComponent
       }, {
         path: 'perfil', component: PerfilComponent, children: [
-          {path: 'inicio', component: InicioComponent},
-          {path: 'servicios', component: ServiciosComponent},
-          {path: 'citas', component: CitasComponent}
+          {path: '', component: UserServiciosComponent},
+          {path: 'citas', component: UserCitasComponent}
         ]
       },
       {
@@ -76,8 +79,6 @@ const appRoutes: Routes = [
             path: '',
             component: ServiciosPacienteComponent
           },
-          {path: 'resumen', component: ResumenComponent, pathMatch: 'full'},
-          {path: 'anexos', component: AnexosComponent},
           {
             path: 'historia', component: HistoriaComponent, children: [
               {path: 'basico', component: HcBasicComponent},
@@ -167,7 +168,11 @@ export const environment = {
     ItemReferenciaComponent,
     CntComponent,
     ItemDirComponent,
-    AppCheckboxComponent],
+    AppCheckboxComponent,
+    MultipleComponent,
+    UserServiciosComponent,
+    UserCitasComponent,
+    UserResumenComponent],
 
   imports: [RouterModule.forRoot(appRoutes),
     BrowserModule, AngularFireModule.initializeApp(environment.firebase), AngularFireDatabaseModule,
