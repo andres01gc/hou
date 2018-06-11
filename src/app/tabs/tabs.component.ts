37862fce1c2dc93768e0775fc1ac13d24aef7585
gc <anfgc01@gmail.com>
2018-06-11 17:06:28 -0500

    se crea el componente de edición, y se cargan las páginas

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, AfterViewInit, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList} from '@angular/core';
 import {TabComponent} from '../tab/tab.component';
 
 @Component({
@@ -7,8 +7,9 @@ import {TabComponent} from '../tab/tab.component';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements AfterContentInit {
-
+  @Input() agregable = true;
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+  @Output() alAgregar = new EventEmitter();
 
   ngAfterContentInit() {
   }
@@ -16,7 +17,7 @@ export class TabsComponent implements AfterContentInit {
   selectTab(tab: TabComponent) {
     // TODO averiguar, porque se llama este método al crearse este compmonente! :/, tocó crear ese if para evitar el error
     this.tabs.toArray().forEach(tb => tb.tab_activa = false);
-      tab.tab_activa = true;
+    tab.tab_activa = true;
   }
 
 }
